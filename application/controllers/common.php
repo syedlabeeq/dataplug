dfd5909ae48ae4885e36f04bed8e084de0b65086
syedlabeeq <77527678+syedlabeeq@users.noreply.github.com>
2021-01-17 16:47:15 +0500

    function name corrected
    function name was not in camel case.

--- application/controllers/common.php
+++ application/controllers/common.php
@@ -7,7 +7,7 @@ class Common extends CI_Controller {
         parent::__construct();
     }
 
-    public function get_image_to_display(){
+    public function getImageToDisplay(){
         header('Content-Type: image/png');
         $image_name = image_name_decode($_GET['img']);
         readfile($image_name);
